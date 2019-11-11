5e38284644158b1a8d13e769479078c91d018122
rafael-tec <rafaeltecinfo9@gmail.com>
2019-11-11 19:11:46 -0300

    Implementação de método Get por id

--- src/main/java/br/com/catalog/controller/CategoryController.java
+++ src/main/java/br/com/catalog/controller/CategoryController.java
@@ -6,10 +6,12 @@ import br.com.catalog.model.CategoryModel;
 import br.com.catalog.repository.CategoryRepository;
 import br.com.catalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/category")
@@ -23,9 +25,16 @@ public class CategoryController {
         return categoryService.getAll();
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<CategoryResponseDto> getById(@PathVariable Integer id) {
+        return categoryService.getById(id);
+    }
+
     @PostMapping(path = "/")
     public void save(@RequestBody @Valid CategoryRequestDto categoryDto) {
         categoryService.save(categoryDto);
     }
+
+
 
 }
--- src/main/java/br/com/catalog/service/CategoryService.java
+++ src/main/java/br/com/catalog/service/CategoryService.java
@@ -5,9 +5,11 @@ import br.com.catalog.dto.response.CategoryResponseDto;
 import br.com.catalog.model.CategoryModel;
 import br.com.catalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +22,16 @@ public class CategoryService {
         List<CategoryModel> categories = categoryRepository.findAll();
 
         return categories.stream().map(CategoryResponseDto::new).collect(Collectors.toList());
+    }
+
+    public ResponseEntity<CategoryResponseDto> getById(Integer id) {
+        Optional<CategoryModel> optinal = categoryRepository.findById(id);
+
+        if(optinal.isPresent()) {
+            return ResponseEntity.ok(new CategoryResponseDto(optinal.get()));
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     public void save(CategoryRequestDto categoryDto) {
